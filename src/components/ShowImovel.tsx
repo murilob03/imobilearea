50eca6679b8f99a942f7e1da250742ce8410058f
juliana-kawakami <juliana121204@gmail.com>
2025-11-17 16:27:39 -0300

    fix:corrige quebra de layout de preços e nomes grandes

--- src/components/ShowImovel.tsx
+++ src/components/ShowImovel.tsx
@@ -94,7 +94,7 @@ const ShowImovel: React.FC<ShowImovelProps> = ({ imovel }) => {
           />
         </div>
       </div>
-      <div className="absolute bottom-0 w-full bg-gradient-to-t from-marrom to-marrom text-white p-4 h-[75px]">
+      <div className="absolute bottom-0 w-full bg-gradient-to-t from-marrom to-marrom text-white p-4 h-fit">
         <div className="flex justify-between items-center">
           <h3 className="text-base font-bold truncate">{imovel.nome}</h3>
           <h3 className="text-base font-bold truncate">
